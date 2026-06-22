1bb8e01da2cad762cdc24f122fa005db0eb71a84
AsyncIter <intellild@outlook.com>
2026-06-22 19:46:24 +0800

    test: restore define plugin import meta env case

---
+++ tests/rspack-test/configCases/plugins/define-plugin-import-meta/index.js
@@ -0,0 +1,8 @@
+it("should not have import.meta.env", function() {
+	var _env;
+	(_env = import.meta.env) === null || _env === void 0 ? void 0 : _env.production;
+	expect(_env).toEqual({
+		MODE: "production",
+		NODE_ENV: "production"
+	});
+});
